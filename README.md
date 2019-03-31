0d0bf74e6895f9bf3bd9fe7edbc1920ff9086467
iamousseni <ousseni.work@gmail.com>
2019-03-31 04:30:48 +0200

    new updates

--- README.md
+++ README.md
@@ -1,1 +1,190 @@
-# xml
+
+# Documentation
+```
+class Tag {
+    /*properties*/
+    string name;
+    string innerText;
+    array attributes = []; //attribute[name => value]
+    Tag parent;
+    Tag childs; //array of tag
+
+    /*methods*/
+    __construct(string name, string innerText = '', array attributes = [], Tag parent = null, Tag childs = null) :void
+    setName(string name) :void
+    getName() :string
+    setInnerText(innerText) :void
+    getInnerText() :string
+    setAttribute(string name, string value) :void
+    getAttribute() :array
+    setParent(Tag parent) :void
+    getParent() :Tag
+    setChild(Tag child) :void
+    getChilds() :array of Tag
+}
+
+class XML extends Tag{
+    /*properties*/
+    string version;
+    string encoding;
+    Tag root;
+
+    /*methods*/
+    __construct(string version = '1.0', string encoding = 'utf-8' ) :void
+    setVersion(string version) :void
+    getVersion() :string
+    setEncoding(string encoding) :void
+    getEncoding() :string
+    setRoot(string name, string innerText = '', array attributes = [], Tag parent = null, Tag child = null) :void
+    getRoot() :Tag
+    createTag(string name, string innerText = '', array attributes = [], Tag parent = null, Tag child = null) :Tag
+    getXML_string() :string
+    getXML_string_from_array(array dataArray) :string
+    getArray_from_XML(XML Tree) :array
+    getArray_from_XML() :array
+    getXML_string_from_file(string fileName) :string
+    getXML_file_from_string(string xml) :file
+    createXML_file_from_array(array dataArray) :boolean
+    createXML_file_from_XML_OBJ(XML Obj) :boolean
+    createXML_file_from_XMLstring(string XMLstring) :boolean
+}
+```
+
+## How to use
+
+To use this class in your project you will need to include it.
+```code
+include('xml.php');
+```
+
+## Examples to use
+
+# Example 1
+
+```
+$xml = new XML('1.0', 'utf-8');
+$users_array = array(
+  "users" => array(
+      'user1' => array(
+          "id" => 1,
+          "name" => "Smith",
+          "address" => array(
+              "country" => "United Kingdom",
+              "city" => "London",
+              "zip" => 56789,
+          )
+      ),
+      'user2' => array(
+          "id" => 2,
+          "name" => "John",
+          "address" => array(
+              "country" => "USA",
+              "city" => "Newyork",
+              "zip" => "NY1234",
+          ) 
+      ),
+      'user3' => array(
+          "id" => 3,
+          "name" => "Viktor",
+          "address" => array(
+              "country" => "Australia",
+              "city" => "Sydney",
+              "zip" => 123456,
+          ) 
+      ),
+  )
+);
+
+header('Content-Type: text/xml');
+echo $xml->getXML_string_from_array($users_array);
+```
+
+# Example 2
+
+```
+$xml = new XML('1.0', 'utf-8');
+$xml->setRoot('users', '', ['class' => 'main']);
+$user1 = $xml->createTag('user1');
+$id = $xml->createTag('id', '1', [], $user1);
+$name = $xml->createTag('name', 'Viktor', [], $user1);
+header('Content-Type: text/xml');
+echo $xml->getXML_string();
+
+```
+-> example of photo <-
+
+# Example 3
+
+```
+$xml = new XML('1.0', 'utf-8');
+$xml->setRoot('users', '', ['class' => 'main']);
+$user1 = $xml->createTag('user');
+$id = $xml->createTag('id', '1', [], $user1);
+$name = $xml->createTag('name', 'Viktor', [], $user1);
+$xml->createXML_file_from_XML_OBJ();
+
+```
+-> example of photo <-
+
+# Example 4
+
+```
+$xml = new XML('1.0', 'utf-8');
+$users_array = array(
+  "users" => array(
+      'user1' => array(
+          "id" => 1,
+          "name" => "Smith",
+          "address" => array(
+              "country" => "United Kingdom",
+              "city" => "London",
+              "zip" => 56789,
+          )
+      ),
+      'user2' => array(
+          "id" => 2,
+          "name" => "John",
+          "address" => array(
+              "country" => "USA",
+              "city" => "Newyork",
+              "zip" => "NY1234",
+          ) 
+      ),
+      'user3' => array(
+          "id" => 3,
+          "name" => "Viktor",
+          "address" => array(
+              "country" => "Australia",
+              "city" => "Sydney",
+              "zip" => 123456,
+          ) 
+      ),
+  )
+);
+$xml->createXML_file_from_array($users_array);
+```
+-> example of photo <-
+
+# Example 5
+
+```
+$xml = new XML('1.0', 'utf-8');
+$xml->setRoot('users', '', ['class' => 'main']);
+$user1 = $xml->createTag('user1');
+$id = $xml->createTag('id', '1', [], $user1);
+$name = $xml->createTag('name', 'Viktor', [], $user1);
+$xml->createXML_file_from_XMLstring($xml->getXML_string());
+```
+-> example of photo <-
+
+# Example 6
+
+```
+$xml = new XML('1.0', 'utf-8');
+$xml->setRoot('users');
+$user1 = $xml->createTag('user');
+$id = $xml->createTag('id', '1', [], $user1);
+$name = $xml->createTag('name', 'Viktor', [], $user1);
+var_dump($xml->getArray_from_XML());
+```
+-> example of photo <-
--- xml.php
+++ xml.php
@@ -1,83 +1,280 @@
 <?php
-class XML extends Tag{
-    /* Properties */
-    private $version;
-    private $encoding;
-    private $root;
 
-    /* Methods */
-    public function __construct(string $version = '1.0', string $encoding = 'utf-8')
-    {
-        $this->setVersion($version);
-        $this->setEncoding($encoding);
+/*
+    @author Ousseni Bara
+    @github iamousseni
+    version: 1.0.0
+    last modified: 31/03/2018
+*/
+
+class Tag
+{
+  /* Properties */
+  private $name;
+  private $innerText;
+  private $attributes;
+  private $parent;
+  private $childs = [];
+
+  /* Methods */
+  public function __construct(
+    string $name,
+    string $innerText = null,
+    array $attribute = null,
+    $parent = null,
+    $childs = null
+  ) {
+    $this->setName($name);
+    $this->setInnerText($innerText);
+    if ($attribute !== null)
+      $this->setAttribute($attribute);
+    $this->setParent($parent);
+    if ($childs !== null)
+      $this->setChild($childs);
+  }
+
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function setInnerText($innerText)
+  {
+    $this->innerText = $innerText;
+  }
+
+  public function getInnerText()
+  {
+    return $this->innerText;
+  }
+
+  public function setAttribute(array $attribute)
+  {
+    foreach ($attribute as $key => $value) {
+      $this->attributes[$key] = $value;
+    }
+  }
+
+  public function getAttributes()
+  {
+    return $this->attributes;
+  }
+
+  public function setParent($parent)
+  {
+    $this->parent = $parent;
+  }
+
+  public function getParent()
+  {
+    return $this->parent;
+  }
+
+  public function setChild($child)
+  {
+    $this->childs[] = $child;
+  }
+
+  public function getChilds()
+  {
+    return $this->childs;
+  }
+
+  public function generateXML_from_tag()
+  {
+    $xmlString = "";
+    $xmlString .= "<" . $this->getName();
+    if (null !== $this->getAttributes()) {
+      foreach ($this->getAttributes() as $key => $value) {
+        $xmlString .= " $key = \"" . $value . "\"";
+      }
     }
 
-    public function setVersion($version)
-    {
-        $this->version = $version;
+    if (null == $this->getInnerText() && null == $this->getChilds()) {
+      $xmlString .= " />";
+    } else {
+      $xmlString .= ">";
+      $xmlString .= $this->getInnerText();
+      if ($this->getChilds() !== null) {
+        foreach ($this->getChilds() as $child) {
+          $xmlString .= $child->generateXML_from_tag();
+        }
+      }
+      $xmlString .= "</" . $this->getName() . ">";
     }
+    return $xmlString;
+  }
+}
 
-    public function getVersion()
-    {
-        return $this->version;
+class XML extends Tag
+{
+  /* Properties */
+  private $version;
+  private $encoding;
+  private $root;
+
+  /* Methods */
+  public function __construct(string $version = '1.0', string $encoding = 'utf-8')
+  {
+    $this->setVersion($version);
+    $this->setEncoding($encoding);
+  }
+
+  public function setVersion($version)
+  {
+    $this->version = $version;
+  }
+
+  public function getVersion()
+  {
+    return $this->version;
+  }
+
+  public function setEncoding($encoding)
+  {
+    $this->encoding = $encoding;
+  }
+
+  public function getEncoding()
+  {
+    return $this->encoding;
+  }
+
+  public function setRoot(
+    string $name,
+    string $innerText = null,
+    array $attribute = []
+  ) {
+    $this->root = new Tag($name, $innerText, $attribute);
+  }
+
+  public function getRoot()
+  {
+    return $this->root;
+  }
+
+  public function createTag(
+    string $name,
+    string $innerText = '',
+    array $attribute = [],
+    $parent = null,
+    $child = null
+  ) {
+    //if root was setted
+    if ($this->getRoot() != null) {
+      //if parent has been specified
+      if ($parent != null) {
+        //create Tag obj
+        $tag = new Tag($name, $innerText, $attribute, $parent, $child);
+        //set this tag as a child of parent that was specified
+        $parent->setChild($tag);
+        return $tag;
+      } else {
+        $tag = new Tag($name, $innerText, $attribute, $this->getRoot(), $child);
+        $this
+          ->getRoot()
+          ->setChild($tag);
+
+        return $tag;
+      }
+    } else {
+      return false;
     }
+  }
 
-    public function setEncoding($encoding)
-    {
-        $this->encoding = $encoding;
+  public function getXML_string()
+  {
+    if ($this->getRoot() === null)
+      return false;
+    else
+      return $this
+        ->getRoot()
+        ->generateXML_from_tag();
+  }
+
+  private function generateXML($tagName, $tagValue)
+  {
+    $xmlString = '<' . $tagName;
+    if ($tagValue === null) {
+      $xmlString .= '/>';
+    } else {
+      $xmlString .= '>';
+      if (!is_array($tagValue)) {
+        $xmlString .= $tagValue . '</' . $tagName . '>';
+      } else {
+        foreach ($tagValue as $key => $childValue) {
+          $xmlString .= $this->generateXML($key, $childValue);
+        }
+        $xmlString .= '</' . $tagName . '>';
+      }
     }
+    return $xmlString;
+  }
 
-    public function getEncoding()
-    {
-        return $this->encoding;
+  public function getXML_string_from_array(array $arrayData)
+  {
+    $xmlString = '';
+    foreach ($arrayData as $key => $value) {
+      $xmlString .= $this->generateXML($key, $value);
     }
+    return $xmlString;
+  }
 
-    public function setRoot(string $name,
-        string $innerText = '',
-        array $attribute = [],
-        $parent = null,
-        $child = null)
-    {
-        $this->root = $this->createTag($name, $innerText, $attribute, $parent, $child);
+  private function addTagOnXML(array $data)
+  {
+    $xmlArray = [];
+    foreach ($data as $child) {
+      if ($child->getChilds() != null) {
+        $xmlArray[$child->getName()] = $this->addTagOnXML($child->getChilds());
+      } else {
+        $xmlArray[$child->getName()] = $child->getInnerText();
+      }
     }
+    return $xmlArray;
+  }
 
-    public function getRoot()
-    {
-        return $this->root;
+  public function getArray_from_XML()
+  {
+    if ($this->getRoot() !== null) {
+      $xmlArray[$this->getRoot()->getName()] = $this->getRoot()->getInnerText();
+      if ($this->getRoot()->getChilds() !== null) {
+        $xmlArray[$this->getRoot()->getName()] = $this->addTagOnXML($this->getRoot()->getChilds());
+      }
+      return $xmlArray;
+    } else {
+      return false;
     }
+  }
 
-    public function createTag(
-        string $name,
-        string $innerText = '',
-        array $attribute = [],
-        $parent = null,
-        $child = null){
-            //if root was setted
-            if($this->getRoot() != null){
-                //if parent has been specified
-                if($parent != null){
-                    //create Tag obj
-                    $tag = new Tag($name, $innerText, $attribute, $parent, $child);
-                    //set this tag as a child of parent that was specified
-                    $parent->setChild($tag);
-                    return $tag;
-                }else{
-                    $tag = new Tag($name, $innerText, $attribute, $this->getRoot(), $child);
-                    $this
-                        ->getRoot()
-                        ->setChild($tag);
+  public function createXML_file_from_array(array $arrayData)
+  {
+    $xmlFile = fopen('xmlFile.xml', 'w');
+    $status = fwrite($xmlFile, $this->getXML_string_from_array($arrayData));
+    fclose($xmlFile);
+    echo $status !== false ? 'File created successfull!' : 'Error! There was an array on writing into file.';
+    return $status !== false ? true : false;
+  }
 
-                    return $tag;
-                }
-            }else{
-                return false;
-            }
-    }
+  public function createXML_file_from_XMLstring(string $XMLstring)
+  {
+    $xmlFile = fopen('xmlFile.xml', 'w');
+    $status = fwrite($xmlFile, $XMLstring);
+    fclose($xmlFile);
+    echo $status !== false ? 'File created successfull!' : 'Error! There was an array on writing into file.';
+    return $status !== false ? true : false;
+  }
 
-    public function getXML_string(){
-        
-    }
-
-
-
+  public function createXML_file_from_XML_OBJ()
+  {
+    $xmlString = $this->getXML_string();
+    if ($xmlString !== false)
+      return  $this->createXML_file_from_XMLstring($xmlString);
+    else
+      return false;
+  }
 }
